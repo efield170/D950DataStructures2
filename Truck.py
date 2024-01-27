29f1504c91b40611b699a077b74a6ad4505237e3
efield1 <efield170@gmail.com>
2024-01-27 15:27:12 -0600

    Update Truck clas

--- Truck.py
+++ Truck.py
@@ -4,5 +4,16 @@ Created on Thu Jan 18 13:31:37 2024
 
 @author: efiel
 """
+from HashMap import HashMap
 
-print(2+2)
+class Truck:
+    
+    def __init__(self):
+        self.miles = 0
+        self.start_time = None
+        self.total_time = None
+        self.air_manifest = HashMap()
+        self.driver_manifest = HashMap()
+        
+        ##### need to make a function that converts miles into time 
+        
--- main.py
+++ main.py
@@ -5,17 +5,18 @@ Created on Thu Jan 18 13:31:58 2024
 @author: efiel
 
 """
+from Truck import Truck
 from HashMap import HashMap
 from Package import Package
 import pandas as pd
 
+### code to read package file, initialzie hash map and populate it###### 
 
 
 package_manifest_df = pd.read_csv('package_file.csv')
 
 package_manifest = []
 
-print(package_manifest_df.head())
 
 for i, row in package_manifest_df.iterrows():
     package_instance = Package(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])
@@ -26,3 +27,19 @@ hash_map_manifest = HashMap()
 for package in package_manifest:
     hash_map_manifest.add(package.GetPackageId(), package)
     
+###################################################################
+
+####   create the three truck instances and load them
+
+truck_one = Truck()
+truck_two = Truck()
+truck_three = Truck()
+
+
+
+
+
+
+
+
+
