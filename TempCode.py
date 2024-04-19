eb4163814c48c47957c58ccfd55f2921a09c145f
efield1 <efield170@gmail.com>
2024-04-19 17:37:38 -0500

    Finish algo, begin work on CLI

--- TempCode.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import HashMap as HashMap
-
-distance_table = pd.read_csv('Real_Distance_Table.csv', index_col=0)
-
-print("hi")
--- main.py
+++ main.py
@@ -41,6 +41,7 @@ truck_one = Truck()
 truck_two = Truck()
 truck_three = Truck()
 truck_one.start_time = dt.datetime(2024, 4, 19, 8, 0 ,0)
+truck_two.start_time = dt.datetime(2024, 4, 19, 8, 0 ,0)
 
 #print(f'current location {truck_one.current_location}')
 #print(truck_one.find_next_stop())
@@ -59,16 +60,48 @@ truck_one.air_manifest.add(hash_map_manifest.get(39).GetPackageId(), hash_map_ma
 truck_one.air_manifest.add(hash_map_manifest.get(8).GetPackageId(), hash_map_manifest.get(8))
 truck_one.air_manifest.add(hash_map_manifest.get(30).GetPackageId(), hash_map_manifest.get(30))
 truck_one.air_manifest.add(hash_map_manifest.get(5).GetPackageId(), hash_map_manifest.get(5))
-truck_one.air_manifest.add(hash_map_manifest.get(9).GetPackageId(), hash_map_manifest.get(9))
 truck_one.air_manifest.add(hash_map_manifest.get(37).GetPackageId(), hash_map_manifest.get(37))
 truck_one.air_manifest.add(hash_map_manifest.get(1).GetPackageId(), hash_map_manifest.get(1))
 truck_one.air_manifest.add(hash_map_manifest.get(14).GetPackageId(), hash_map_manifest.get(14))
 
-print(truck_one.driver_manifest.print_map())
+#print(truck_one.driver_manifest.print_map())
 
 truck_one.driver_manifest.add(hash_map_manifest.get(19).GetPackageId(), hash_map_manifest.get(19))
 truck_one.driver_manifest.add(hash_map_manifest.get(17).GetPackageId(), hash_map_manifest.get(17))
+truck_one.driver_manifest.add(hash_map_manifest.get(10).GetPackageId(), hash_map_manifest.get(10))
 
+### POPULATE TRUCK 2###
+
+truck_two.air_manifest.add(hash_map_manifest.get(6).GetPackageId(), hash_map_manifest.get(6))
+truck_two.air_manifest.add(hash_map_manifest.get(31).GetPackageId(), hash_map_manifest.get(31))
+truck_two.air_manifest.add(hash_map_manifest.get(26).GetPackageId(), hash_map_manifest.get(26))
+truck_two.air_manifest.add(hash_map_manifest.get(29).GetPackageId(), hash_map_manifest.get(29))
+
+
+truck_two.driver_manifest.add(hash_map_manifest.get(3).GetPackageId(), hash_map_manifest.get(3))
+truck_two.driver_manifest.add(hash_map_manifest.get(18).GetPackageId(), hash_map_manifest.get(18))
+truck_two.driver_manifest.add(hash_map_manifest.get(38).GetPackageId(), hash_map_manifest.get(38))
+truck_two.driver_manifest.add(hash_map_manifest.get(36).GetPackageId(), hash_map_manifest.get(36))
+truck_two.driver_manifest.add(hash_map_manifest.get(32).GetPackageId(), hash_map_manifest.get(32))
+truck_two.driver_manifest.add(hash_map_manifest.get(26).GetPackageId(), hash_map_manifest.get(26))
+truck_two.driver_manifest.add(hash_map_manifest.get(7).GetPackageId(), hash_map_manifest.get(7))
+truck_two.driver_manifest.add(hash_map_manifest.get(22).GetPackageId(), hash_map_manifest.get(22))
+truck_two.driver_manifest.add(hash_map_manifest.get(24).GetPackageId(), hash_map_manifest.get(24))
+truck_two.driver_manifest.add(hash_map_manifest.get(23).GetPackageId(), hash_map_manifest.get(23))
+truck_two.driver_manifest.add(hash_map_manifest.get(11).GetPackageId(), hash_map_manifest.get(11))
+truck_two.driver_manifest.add(hash_map_manifest.get(12).GetPackageId(), hash_map_manifest.get(12))
+
+
+
+### POPULATE TRUCK 3###
+truck_three.driver_manifest.add(hash_map_manifest.get(9).GetPackageId(), hash_map_manifest.get(9))
+truck_three.driver_manifest.add(hash_map_manifest.get(40).GetPackageId(), hash_map_manifest.get(40))
+truck_three.driver_manifest.add(hash_map_manifest.get(28).GetPackageId(), hash_map_manifest.get(28))
+truck_three.driver_manifest.add(hash_map_manifest.get(2).GetPackageId(), hash_map_manifest.get(2))
+truck_three.driver_manifest.add(hash_map_manifest.get(4).GetPackageId(), hash_map_manifest.get(4))
+truck_three.driver_manifest.add(hash_map_manifest.get(27).GetPackageId(), hash_map_manifest.get(27))
+truck_three.driver_manifest.add(hash_map_manifest.get(33).GetPackageId(), hash_map_manifest.get(33))
+truck_three.driver_manifest.add(hash_map_manifest.get(35).GetPackageId(), hash_map_manifest.get(35))
 
 #print(truck_one.driver_manifest.print_map())
 #print(truck_one.air_manifest.print_map())
@@ -107,12 +140,23 @@ truck_one.driver_manifest.add(hash_map_manifest.get(17).GetPackageId(), hash_map
 # print(f'current location {truck_one.current_location}')
 
 truck_one.run_route()
+truck_two.run_route()
+truck_three.start_time = truck_two.start_time + truck_two.total_time_elapsed
+truck_three.run_route()
 #truck_one.complete_stop()
 #truck_one.complete_stop()
 
 print(truck_one.miles_driven)
 print(truck_one.total_time_elapsed)
 truck_one.print_package_tracking_info()
+
+print(truck_two.miles_driven)
+print(truck_two.total_time_elapsed)
+truck_two.print_package_tracking_info()
+
+print(truck_three.miles_driven)
+print(truck_three.total_time_elapsed)
+truck_three.print_package_tracking_info()
 
 ####################################################################
 
