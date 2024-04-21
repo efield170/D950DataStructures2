79587ccf2237a9ad4c7a21c5b50c5cf086a1dbdc
efield1 <efield170@gmail.com>
2024-04-21 07:14:44 -0500

    Finish CLI wihtout showing milage at time

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ import datetime as dt
 
 
 def main():
+    
+    package_tracking_manifest = HashMap()
+
 
     package_manifest_df = pd.read_csv('package_file.csv', header= None)
     
@@ -101,32 +104,97 @@ def main():
     truck_three.driver_manifest.add(hash_map_manifest.get(35).GetPackageId(), hash_map_manifest.get(35))
     
     #Prompt needed info from user
+    print("Welcom to the WGUPS Delivery Tracking Portal")
+    current_time = input("What is the current time? (HH:MM): ")
+    chosen_time = dt.datetime.strptime(current_time, "%H:%M")
+    package_to_track = (input("Enter the Package ID for the chose package or press Enter for all packages "))
+    
     
     
     
     truck_one.run_route()
     truck_two.run_route()
-    truck_three.start_time = truck_two.start_time + truck_two.total_time_elapsed
+    
+    truck_three_earliest_start = dt.datetime(2024,4,19,10,20)
+    
+    if truck_two.start_time + truck_two.total_time_elapsed >= truck_three_earliest_start:
+        truck_three.start_time = truck_two.start_time + truck_two.total_time_elapsed
+    else:
+        truck_three.start_time = truck_three_earliest_start
+        
     truck_three.run_route()
     
-    package_tracking_manifest = HashMap()
+    for key, value in truck_one.package_status:
+        delivery_time = value[2].time()
+        if delivery_time <= chosen_time.time():
+            location = "Delivered"
+            delivery_time = value[2]
+        else:
+            location = "En Route"
+            delivery_time = "TBD"
+        
+        package_tracking_manifest.add(key, [key, location, delivery_time])
+        
+    for key, value in truck_two.package_status:
+        delivery_time = value[2].time()
+        if delivery_time <= chosen_time.time():
+            location = "Delivered"
+            delivery_time = value[2]
+        else:
+            location = "En Route"
+            delivery_time = "TBD"
+        
+        package_tracking_manifest.add(key, [key, location, delivery_time])
+        
+    for key, value in truck_three.package_status:
+        delivery_time = value[2].time()
+        if delivery_time <= chosen_time.time():
+            location = "Delivered"
+            delivery_time = value[2]
+        if chosen_time.time() < truck_three.start_time.time():
+            location = "HUB"
+            delivery_time = "TBD"
+        else:
+            location = "En Route"
+            delivery_time = "TBD"
+        
+        package_tracking_manifest.add(key, [key, location, delivery_time])
     
     
+   # package_tracking_manifest.print_map()
     
+    if package_to_track == "":
+        for key, value in package_tracking_manifest:
+            print(f'Package ID: {value[0]}')
+            print(f'    Package Status/location: {value[1]}')
+            print(f'    Delivery Time: {value[2]}')
+            print()
+            
+    else:
+        if int(package_to_track) in package_tracking_manifest:
+            for key, value in package_tracking_manifest:
+                if key == int(package_to_track):
+                     print(f'Package ID: {value[0]}')
+                     print(f'    Package Status/location: {value[1]}')
+                     print(f'    Delivery Time: {value[2]}')
+                     print()
+        else:
+            print("Sorry, that package was not found in todays deliveries.")
+            
+    print(truck_three.start_time)
+
     
+   # print(truck_one.miles_driven)
+    #print(truck_one.total_time_elapsed)
+  #  truck_one.print_package_tracking_info()
     
+   # print(truck_two.miles_driven)
+  #  print(truck_two.total_time_elapsed)
+    #truck_two.print_package_tracking_info()
     
-    print(truck_one.miles_driven)
-    print(truck_one.total_time_elapsed)
-    truck_one.print_package_tracking_info()
-    
-    print(truck_two.miles_driven)
-    print(truck_two.total_time_elapsed)
-    truck_two.print_package_tracking_info()
-    
-    print(truck_three.miles_driven)
-    print(truck_three.total_time_elapsed)
-    truck_three.print_package_tracking_info()
+    #print(truck_three.miles_driven)
+    #print(truck_three.total_time_elapsed)
+    #truck_three.print_package_tracking_info()
     
     ####################################################################
     
